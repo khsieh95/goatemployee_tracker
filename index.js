562d0d1aa302d2f89cbf4a6f33f1cefca7bc6f5a
Kevin Hsieh <khsieh95@gmail.com>
2021-01-20 15:38:40 -0800

    created starting async function to prompt

---
+++ index.js
@@ -0,0 +1,53 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "employee_tracker_db",
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "start",
+        choices: [
+          "Add department, role, or employee",
+          "View departments, roles, employees",
+          "Update employee roles",
+          "Quit",
+        ],
+      },
+    ])
+    .then(function (ans) {
+      if (ans.start === "Add department, role, or employee") {
+        console.log("add someone");
+      } else if (ans.start === "View departments, roles, employees") {
+        console.log("View everything");
+      } else if (ans.start === "Update employee roles") {
+        console.log("Update things");
+      } else {
+        console.log("Goodbye!");
+        connection.end();
+      }
+    });
+}
